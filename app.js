1205a564bc6480e5d23b2f9f1a59e6d514f292e8
Tassos Lambrou <lambrou@gmail.com>
2018-03-19 01:27:45 -0700

    reorganized

---
+++ app.js
@@ -0,0 +1,13 @@
+
+//**** DEPENDENCIES ****//
+const express = require('express');
+const app = express();
+
+//**** MIDDLEWARE ****//
+app.use(express.static('public'))
+
+var PORT = process.env.PORT || 8000;
+
+app.listen(PORT, function(req, res) {
+  console.log("Tassos.net App listening on port " + PORT + "...");
+});
