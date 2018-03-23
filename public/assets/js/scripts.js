cc924ae5ef608b37755177cc7598f360e4104b54
Tassos Lambrou <lambrou@gmail.com>
2018-03-22 23:58:11 -0700

    Fixes to buttons at the bottom and sidebar didn't full show correctly as well as preload animation fix

--- public/assets/js/scripts.js
+++ public/assets/js/scripts.js
@@ -1,6 +1,69 @@
 
 $(document).ready(function() {
 
+  const scrollReveal = () => {// Select all links with hashes
+    $('a[href*="#"]')
+    // Remove links that don't actually link to anything
+    .not('[href="#"]')
+    .not('[href="#0"]')
+    .click(function(event) {
+      // On-page links
+      if (
+        location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+        &&
+        location.hostname == this.hostname
+      ) {
+        // Figure out element to scroll to
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+        // Does a scroll target exist?
+        if (target.length) {
+          // Only prevent default if animation is actually gonna happen
+          event.preventDefault();
+          $('html, body').animate({
+            scrollTop: target.offset().top
+          }, 1000, function() {
+            // Callback after animation
+            // Must change focus!
+            var $target = $(target);
+            $target.focus();
+            if ($target.is(":focus")) { // Checking if the target was focused
+              return false;
+            } else {
+              $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+              $target.focus(); // Set focus again
+            };
+          });
+        }
+      }
+    });
+
+    window.sr = ScrollReveal();
+    // sr.reveal('.card');
+    sr.reveal('.profile-summary');
+    // sr.reveal('.grid');
+    sr.reveal('.reveal');
+  }
+
+  scrollReveal();
+
+  // TYPED.JS
+  // var typed = new Typed('.typed', {
+  //   stringsElement: '#typed-strings'
+  // });
+})
+
+window.onload = () => {
+  var lineDrawing = anime({
+    targets: '#lineDrawing .lines path',
+    strokeDashoffset: [anime.setDashoffset, 0],
+    easing: 'easeInBack',
+    duration: 800,
+    delay: function(el, i) { return i * 200 },
+    direction: 'alternate',
+    loop: false
+  });
+
   $.fn.extend({
     animateCss: function(animationName, callback) {
       var animationEnd = (function(el) {
@@ -28,65 +91,6 @@ $(document).ready(function() {
     },
   });
 
-  // Select all links with hashes
-  $('a[href*="#"]')
-  // Remove links that don't actually link to anything
-  .not('[href="#"]')
-  .not('[href="#0"]')
-  .click(function(event) {
-    // On-page links
-    if (
-      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
-      &&
-      location.hostname == this.hostname
-    ) {
-      // Figure out element to scroll to
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-      // Does a scroll target exist?
-      if (target.length) {
-        // Only prevent default if animation is actually gonna happen
-        event.preventDefault();
-        $('html, body').animate({
-          scrollTop: target.offset().top
-        }, 1000, function() {
-          // Callback after animation
-          // Must change focus!
-          var $target = $(target);
-          $target.focus();
-          if ($target.is(":focus")) { // Checking if the target was focused
-            return false;
-          } else {
-            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
-            $target.focus(); // Set focus again
-          };
-        });
-      }
-    }
-  });
-
-  var lineDrawing = anime({
-    targets: '#lineDrawing .lines path',
-    strokeDashoffset: [anime.setDashoffset, 0],
-    easing: 'easeInBack',
-    duration: 800,
-    delay: function(el, i) { return i * 200 },
-    direction: 'alternate',
-    loop: false
-  });
-
   // $('#title').animateCss('fadeInDown');
   $('#subtitle').animateCss('flipInX');
-
-  window.sr = ScrollReveal();
-  // sr.reveal('.card');
-  sr.reveal('.profile-summary');
-  // sr.reveal('.grid');
-  sr.reveal('.reveal');
-
-
-
-  // var typed = new Typed('.typed', {
-  //   stringsElement: '#typed-strings'
-  // });
-})
+}
